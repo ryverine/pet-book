edbf6f477488a1d3a9d77fa92123040f1b178831
ryverine <rwcheek@gmail.com>
2019-04-27 11:52:20 -0400

    Adding logged-in user info to database as user object.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,19 @@
-// ryan's js
+
 
 $(document).ready(function() 
 {
+	/*** GLOBAL ***/
+
 	var signInArea = $("#signInArea");
 	var mainContentArea = $("#mainContentArea");
 
-	var defaultGoogleUser = {	displayName: "Unknown", 
-								email: "unknown@unknown.unknown", 
-								photoURL: "assets/images/tmpProfileImg.png"};
+	var defaultGoogleUser = {	
+		displayName: "Unknown", 
+		email: "unknown@unknown.unknown", 
+		photoURL: "assets/images/tmpProfileImg.png"};
 
 	var googleUser = defaultGoogleUser;
 
-  // Initialize Firebase
 	var config = {
 		apiKey: "AIzaSyCW7EalByCr2vcv91omxvV58cuGC2OFWzc",
 		authDomain: "pet-book-ab160.firebaseapp.com",
@@ -25,6 +27,8 @@ $(document).ready(function()
 
 	var database = firebase.database();
 
+	/*** FUNCTIONS ***/
+
 	function updateUserInfo (theUser)
 	{
 		$("#googleDisplayName").text(theUser.displayName);
@@ -34,18 +38,66 @@ $(document).ready(function()
 		profileImg.attr("src", theUser.photoURL);
 
 		$("#profileImageArea").html(profileImg);
+
+		// ADD USER TO FIREBASE
+		storeDataInFirebase(theUser);
 	}
+
+	
+	function storeDataInFirebase(theUser)
+	{
+		// https://firebase.google.com/docs/database/admin/structure-data
+		// https://firebase.google.com/docs/database/admin/save-data
+		// https://firebase.google.com/docs/database/admin/retrieve-data
+
+		var userName = theUser.displayName;
+		var userEmail = theUser.email;
+		var userAddedDateTime = moment().format("MM/DD/YYYY HH:mm:ss");
+		var userLoginDateTime = moment().format("MM/DD/YYYY HH:mm:ss");
+
+		var userPetName = "dog 1";
+		var userPetAge = 3;
+		var userPetWeight = 10.5;
+
+		//var userRef = .child("users")
+		database.ref().child("user").push(
+		{
+			name: userName,
+			email: userEmail,
+			userAdded: userAddedDateTime,
+			lastLogin: userLoginDateTime,
+			pet:{
+				name: userPetName,
+				age: userPetAge,
+				weight: userPetWeight
+			}
+		});
+
+		/*
+		var usersRef = ref.child("users");
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});
+		*/
+	}
+
+
 
 	function googleSignIn()
 	{
-		// https://firebase.google.com/docs/reference/js/firebase.auth.GoogleAuthProvider
 		var provider = new firebase.auth.GoogleAuthProvider();
 		firebase.auth().useDeviceLanguage();
 
 		provider.addScope("profile");
 		provider.addScope("email");
 
-		// https://firebase.google.com/docs/reference/js/firebase.auth.Auth.html#signinwithpopup
 		return firebase.auth().signInWithPopup(provider).then(function(result)
 		{
 			//var token = result.credential.accessToken;
@@ -54,9 +106,13 @@ $(document).ready(function()
 
 			signInArea.hide();
 			mainContentArea.show();
+
+			$('.amazon-stuff').show();
+			$('body').append('<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=cb16da6f-a242-41e1-b8b6-27ccbbf85082"></script>');
 			$("#userProfileArea").show();
+
 			updateUserInfo(result.user);
-		
+
 		}).catch(function(error)
 		{
 			console.log("Google sign-in error: " + "\n" +  error);
@@ -69,8 +125,41 @@ $(document).ready(function()
 		firebase.auth().signOut();
 		updateUserInfo(defaultGoogleUser);
 		signInArea.show();
+
+		$('.amazon-stuff').hide();
+		$('body').find('script').attr('src', '//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=cb16da6f-a242-41e1-b8b6-27ccbbf85082').remove();
 		mainContentArea.hide();
 	}
+
+
+
+
+
+
+
+	/*** PAGE EVENTS ***/
+
+	$("#btn-googleSignIn").on("click", function()
+	{
+		var tmp = googleSignIn();
+	});
+
+	$("#btn-noSignIn").on("click", function()
+	{	
+		signInArea.hide();
+		mainContentArea.show();
+		$('.amazon-stuff').show();
+		$('body').append('<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=cb16da6f-a242-41e1-b8b6-27ccbbf85082"></script>');
+		$("#userProfileArea").show();
+		updateUserInfo(defaultGoogleUser);
+	});
+
+	$("#btn-googleSignOut").on("click", function()
+	{
+		googleSignOut();
+	});
+
+	/*** DATABSE LISTENERS ***/
 
 	database.ref().on("child_added", function(childSnapshot) 
 	{
@@ -80,27 +169,8 @@ $(document).ready(function()
 	console.log("Errors handled: " + errorObject.code);
 	});
 
- 	// user signs-in with google account
-	$("#btn-googleSignIn").on("click", function()
-	{
-		console.log("SIGN IN CLICKED");
-		// https://firebase.google.com/docs/reference/js/firebase.User
-		var tmp = googleSignIn();
-		//console.log("tmp:" + "\n" + tmp);
-	});
 
-	$("#btn-noSignIn").on("click", function()
-	{	
-		console.log("NO GOOGLE CLICKED");
-		signInArea.hide();
-		mainContentArea.show();
-		$("#userProfileArea").show();
-		updateUserInfo(defaultGoogleUser);
-	});
 
-	$("#btn-googleSignOut").on("click", function(){
-		googleSignOut();
-	});
 
  });
 
